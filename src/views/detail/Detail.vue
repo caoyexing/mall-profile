9d29a020a14b5ec27e04b11bb060f6ee12f0f47b
yexingcao0831 <9048910+yexingcao0831@user.noreply.gitee.com>
2022-05-02 13:14:34 +0800

    5.2

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -4,19 +4,21 @@
     <detail-nav></detail-nav>
     <!-- 轮播图 -->
     <swiper :bannersList= 'bannersList'></swiper>
-    
+    <detail-title-info ></detail-title-info>
   </div>
 </template>
 
 <script>
 import DetailNav from './childComps/detailNav.vue'
 import Swiper from './childComps/detailSwiper.vue'
+import DetailTitleInfo from './childComps/detailTitleInfo.vue'
 
 import {getDetailDataById} from '@/network/detail.js'
 export default {
   components:{
     DetailNav,
     Swiper,
+    DetailTitleInfo
   },
   data(){
     return {
@@ -26,15 +28,18 @@ export default {
   },
   created(){
     this.goodsId = this.$route.params.iid 
-    this.getDetailData(this.goodsId)
+    // this.getDetailData(this.goodsId)
   },
   methods:{
     // detail页面信息
     getDetailData(id){
       getDetailDataById(id).then((res)=>{
         console.log(res)
+        // 1.轮播图
         this.bannersList = res.result.itemInfo.topImages
-        console.log(this.bannersList)
+        // 2.标题信息
+
+
       })
     }
   }
---
+++ src/views/detail/childComps/detailTitleInfo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="title_info">
+    我是title
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
